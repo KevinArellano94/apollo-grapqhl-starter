9a2a289a55ccdac8233958bf3333dae58403baee
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2025-02-18 22:04:15 -0500

    add base files

---
+++ main.ts
@@ -0,0 +1,56 @@
+import { ApolloServer } from 'npm:@apollo/server';
+import { expressMiddleware } from 'npm:@apollo/server/express4';
+import cors from 'npm:cors';
+import express from "npm:express";
+
+
+const app = express();
+
+const typeDefs = `#graphql
+	# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+	# This "Book" type defines the queryable fields for every book in our data source.
+	type Book {
+		title: String
+		author: String
+	}
+
+	# The "Query" type is special: it lists all of the available queries that
+	# clients can execute, along with the return type for each. In this
+	# case, the "books" query returns an array of zero or more Books (defined above).
+	type Query {
+		books: [Book]
+	}
+`;
+
+const books = [
+	{
+		title: 'The Awakening',
+		author: 'Kate Chopin',
+	},
+	{
+		title: 'City of Glass',
+		author: 'Paul Auster',
+	},
+];
+
+const resolvers = {
+	Query: {
+		books: () => books,
+	},
+};
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+await server.start();
+
+app.use(
+	'/graphql',
+	cors<cors.CorsRequest>(),
+	express.json(),
+	expressMiddleware(server),
+);
+
+app.listen(8080)
